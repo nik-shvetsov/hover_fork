03d761167e7fde5ab01ff394973bd4d14f6f8730
nikita.shvetsov <nikita.shvetsov@uit.no>
2020-11-10 13:12:14 +0100

    Upd get_checkpoints arguments

--- src/misc/get_checkpoints.py
+++ src/misc/get_checkpoints.py
@@ -1,9 +1,10 @@
 import json
 import os
+import sys
 
-def show_best_checkpoint(path, compare_value, comparator):
+def show_best_checkpoint(path, compare_value='epoch_num', comparator='max'):
     with open(os.path.join(path, "stats.json"), "r") as read_file:
-        data = json.load(read_file) 
+        data = json.load(read_file)
         checkpoints = [epoch_stat[compare_value] for epoch_stat in data]
         if comparator is 'max':
             chckp = max((checkpoints, i) for i, checkpoints in enumerate(checkpoints))
@@ -12,5 +13,10 @@ def show_best_checkpoint(path, compare_value, comparator):
         return (chckp, data)
 
 if __name__ == '__main__':
-    chkp, data = show_best_checkpoint('./', 'epoch_num', 'max')
+    if sys.argv[1]:
+        chkp, data = show_best_checkpoint(sys.argv[1])
+    elif sys.argv[2]:
+        chkp, data = show_best_checkpoint(sys.argv[1], sys.argv[2])
+    elif sys.argv[3]:
+        chkp, data = show_best_checkpoint(sys.argv[1], sys.argv[2], sys.argv[3])
     print(data[chkp[1]])
